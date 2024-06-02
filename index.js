882c6f5a599a052b2321dfa7d7f72d0ffdbfbf8c
fizasarfraz24001 <fizasarfraz24001@gmail.com>
2024-06-02 14:21:53 +0500

    simple calculator

---
+++ index.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter 1st number", type: "number", name: "firstnumber" },
+    { message: "enter 2nd number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+console.log(answer);
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
